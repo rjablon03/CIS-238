7f8b3f43ea3f6e06bba994203f40fef76a4f7d79
rjablon03 <secrets_crasser.0l@icloud.com>
2024-04-01 14:15:47 -0400

    Continued week 12 fitness assignment

--- Week 12/fitness/scripts/script.js
+++ Week 12/fitness/scripts/script.js
@@ -4,6 +4,11 @@
     Date: 4/1/24
 */
 
+// Global Variables
+const video = document.getElementById("example");
+const videoSource = document.getElementById("vid-src")
+const descriptionSource = document.getElementById("despsrc");
+
 // Hamburger menu fuction
 function hamburger() {
     const menu = document.getElementById("menu-links");
@@ -18,4 +23,34 @@ function hamburger() {
         menu.style.display = "block";
         logo.style.display = "none";
     }
+}
+
+// Function to display the burpees example video
+function burpees() {
+    videoSource.src = "media/burpees.mp4";
+    descriptionSource.src = "media/burpees-descriptions.vtt";
+    video.style.display = "block";
+    video.load();
+}
+
+function plank() {
+    videoSource.src = "media/plank.mp4";
+    descriptionSource.src = "media/plank-descriptions.vtt";
+    video.style.display = "block";
+    video.load();
+}
+
+function mountain() {
+    videoSource.src = "media/mc.mp4";
+    descriptionSource.src = "media/mountain-descriptions.vtt";
+    video.style.display = "block";
+    video.load();
+}
+
+// Function to display a promo code
+function discount() {
+    const promo = document.getElementById("special");
+    promo.firstChild.nodeValue = "Promo Code: D25START";
+    promo.style.color = "#ff0000";
+    promo.style.fontSize = "2em";
 }
